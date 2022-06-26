045be41ee71fee7e83b34be58c1cb04d66c74877
Drago <68302450+Dragonlord1005@users.noreply.github.com>
2022-06-25 20:53:18 -0600

    Big brain

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <img alt="Vue logo" src="./assets/logo.png" class="inline-flex"/>
+  <div
+        class="sidebar"
+        className="fixed top-0 left-0 h-screen w-20 m-0 flex flex-col bg-gray-900 text-white shadow-lg"
+    >
+        <nav>
+            <ul>
+                <li>
+                    <router-link to="/">Home</router-link>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <router-view />
 </template>
 
 <style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from "vue-router";
 import App from './App.vue'
+import Home from './pages/Home.vue'
+import './index.css'
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/', component: Home },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes,
+});
+
+
+createApp(App)
+
+.use(router)
+
+.mount("#app");
